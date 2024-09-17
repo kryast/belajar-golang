3a1b4cb864e40828cac1027a25baad7d692d2ddb
Ahmad Syarifuddin <oden.psht@gmail.com>
2024-09-17 15:45:07 +0700

    tipe data string

---
+++ string/string.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("ini adalah string pakai petik 2")
+	fmt.Println("Ahmad")
+	fmt.Println("Ahmad Syarifuddin")
+	fmt.Println(len("Ahmad Syarifuddin")) // len untuk menghitung panjang karakter string termasuk spasi
+}
