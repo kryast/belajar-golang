1787cba2944c5206fcba98e2b4b2c39e96ccd95b
Ahmad Syarifuddin <oden.psht@gmail.com>
2024-09-18 11:25:29 +0700

    operasi perbandingan

---
+++ operasi-perbandingan/operasi-perbandingan.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var nama string = "Ahmad"
+	var name string = "Ahmad"
+
+	var hasil bool = nama == name
+	fmt.Println(hasil)
+
+	var nilai1 = 10
+	var nilai2 = 20
+
+	fmt.Println(nilai1 > nilai2)
+	fmt.Println(nilai1 < nilai2)
+	fmt.Println(nilai1 == nilai2)
+	fmt.Println(nilai1 != nilai2)
+
+}
